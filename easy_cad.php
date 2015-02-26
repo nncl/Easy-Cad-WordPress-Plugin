32be207fec34b639d011cf539ca95eb5999e71df
nncl <nncl@live.com>
2015-02-26 17:05:10 -0300

    Main page

---
+++ easy_cad.php
@@ -0,0 +1,10 @@
+<?php 
+    /*
+    Plugin Name: Easy Cad
+    Plugin URI: http://www.cauealmeida.com/easy-cad
+    Description: Plugin WordPress de cadastro de banners.
+    Author: Cauê Almeida
+    Version: 1.0
+    Author URI: http://www.cauealmeida.com
+    */
+?>
